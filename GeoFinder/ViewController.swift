2dd5981e51755542fb5ee7c6baa92c5ec7a9689b
Fabien Hoarau <fabienmi.hoarau@gmail.com>
2019-11-02 14:49:21 +0400

    Initial Commit

---
+++ GeoFinder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GeoFinder
+//
+//  Created by Fabien HOARAU on 11/2/19.
+//  Copyright © 2019 Fabien HOARAU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
