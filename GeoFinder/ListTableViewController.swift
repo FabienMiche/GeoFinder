4a0a3f24868cd1a5e7593d0dfdba8fc991248182
Fabien Hoarau <fabienmi.hoarau@gmail.com>
2019-11-03 22:37:03 +0400

    Ajout du fichier ListTableViewController permettant de consulter la liste des geocaches (nom, coordonnées, description) Suppression de l'ancien fichier ListViewController : le fichier ListTableViewController le remplace

---
+++ GeoFinder/ListTableViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ListTableViewController.swift
+//  GeoFinder
+//
+//  Created by Fabien HOARAU on 11/3/19.
+//  Copyright © 2019 Fabien HOARAU. All rights reserved.
+//
+
+import UIKit
+
+var geoList = ["geo1", "geo2", "geo3"]
+var longitude = [1.0, 2.0, 3.0]
+var latitude = [2.0, 4.0, 5.5]
+var description = ["Description de la geo1", "Description de la geo2", "Description de la geo3"]
+
+class ListTableViewController: UITableViewController {
+
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        tabBarItem = UITabBarItem(title: NSLocalizedString("list", comment: "List"), image: UIImage(named: "liste"), tag: 2)
+    }
+    
+
+    // MARK: - Table view data source
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return geoList.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = geoList[indexPath.row]
+
+        return cell
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
